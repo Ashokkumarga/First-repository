3ae72c31cccd6579651d385d938388704b472b6e
ashokkumarga <ashokkumarga0@gmail.com>
2022-10-27 19:29:21 +0530

    js

---
+++ freecodecamp project/javascript/ak.js
@@ -0,0 +1,8 @@
+var digit=209;
+
+if(digit%2==0){
+    console.log("The Given Digit is Even Number")
+}
+else{
+    console.log("The Given Digit is Odd Number")
+}
---
+++ freecodecamp project/javascript/assignment1.js
@@ -0,0 +1,142 @@
+// Conditional statements(if , if else)
+
+//writer a program to check if number is divibl by 7 or not?
+
+/*var a=25;
+
+if(a%7==0){
+    console.log("The given number is divible by 7")
+}
+else{
+    console.log("The given number is not divible by 7")
+}*/
+    
+
+//Write a program to check if a number is multiple of 3 or not?
+
+/*
+var b=218;
+if(b%3==0){
+    console.log("The given number is multiple of 3")
+}
+else{
+    console.log("The given number is not multiple of 3")
+}*/
+
+// write a program to check if a number is postive or not?
+ /*
+let a=1;
+
+if(a>0){
+    console.log("The number is postive")
+}
+else{
+    console.log("The number is negative")
+}
+*/
+
+//write a program to check if a number is 3-digit or not?
+/*
+var a=99;
+if(a>99 && a<1000){
+    console.log("3 digit number")
+}
+else{
+    console.log("Not a 3 digit number")
+}*/
+
+//Write a program to check if a given number is even or odd?
+
+/*
+var a=51;
+if(a%2==0){
+    console.log("The given number is even number")
+}
+else{
+    console.log("The given number is Odd number")
+}*/
+
+// Write a program to print the greatest number in given two numbers?
+
+/*
+var a=400;
+var b=200;
+if(a>b){
+    console.log(a)
+}
+else{
+    console.log(b)
+}*/
+
+//Write a program to pint the least number in given two numbers?
+
+/*
+var a=400;
+var b=200;
+if(a<b){
+    console.log(a)
+}
+else{
+    console.log(b)
+}
+*/
+
+//Write a program to print greatest number in the given three numbers?
+/*
+var a=7;
+var b=9;
+var c=2;
+if(a>b&&b>c&&c>a){
+    console.log(a||b||c)
+}
+*/
+
+//Iterative Statements
+
+// write a program to print 1 to 10 using while loop?
+/*
+var i=1;
+while(i<=10){
+    console.log(i)
+    i=i+1;
+}*/
+
+// write a program to print 10 to 1 using while loop?
+/*
+var i=10;
+while(i>=1){
+    console.log(i)
+    i=i-1;
+}
+*/
+
+//Write a program to print 5 to 15 using for loop?
+
+/*
+for(i=15;i>=10;i--){
+    console.log(i)
+} */
+
+//Write a program to print 1 to 10 even numbers?
+/*
+var i=0;
+while(i<=10){
+    console.log(i)
+    i=i+2
+}
+*/
+
+//Write a program to print 1 to 10 even numbers?
+/*
+for(i=1;i<=10;i=i+2){
+    console.log(i)
+}*/
+
+//write a program first 10 multiples of 4 using while loop?
+
+var i=0;
+while(i<=40){
+    console.log(i)
+    i=i+4
+
+}
